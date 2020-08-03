5dc61c892bae210b3c802405fa8686b795b96e5e
omirbeck <ommirtull@gmail.com>
2020-08-04 01:28:07 +0600

    Initial commit

---
+++ src/components/Categories.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+export default ({ items, onClick }) => {
+    const [activeItem, setActiveItem] = useState(null);
+
+    const onSelectItem = (index) => {
+        setActiveItem(index);
+    }
+
+    return (
+        <div className="categories">
+            <ul>
+                <li className={activeItem === null ? 'active' : ''} onClick={() => setActiveItem(null)}>Все</li>
+                {items &&
+                    items.map((name, index) => (
+                        <li
+                            className={activeItem === index ? 'active' : ''}
+                            onClick={() => onSelectItem(index)}
+                            key={`${name}_${index}`}>
+                            {name}
+                        </li>
+                    ))}
+            </ul>
+        </div>
+    );
+}
