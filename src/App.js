5afa7fd3f1b951010d9b5e56b3e7b7d03a404b22
omirbeck <ommirtull@gmail.com>
2020-08-04 15:56:07 +0600

    feat: add PizzaBlock components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,18 @@ import { Header } from './components';
 import { Home, Cart } from './pages';
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('http://localhost:3000/db.json')
+      .then(response => response.json())
+      .then(data =>  setPizzas(data.pizzas))
+      .catch(err => console.log(err))  
+    
+  }, [])
+
+  console.log(pizzas)
+
   return (
     <div className="wrapper">
       <Header />
